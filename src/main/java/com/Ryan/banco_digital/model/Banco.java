b26ba4c5220374e4d80786fba74e3c328b96b5f2
Ryan <ryansilvadeoliveira53@gmail.com>
2025-10-03 22:22:56 -0300

    Projeto finalizado porém faltando

--- src/main/java/com/Ryan/banco_digital/model/Banco.java
+++ src/main/java/com/Ryan/banco_digital/model/Banco.java
@@ -8,18 +8,28 @@ public class Banco {
 
     private List<Conta> contas;
 
-    public Banco(){
+    public Banco() {
         this.contas = new ArrayList<>();
     }
 
-    public Conta cadastrarConta(String nome, String cpf){
+    public Conta cadastrarConta(String nome, String cpf) {
+        if (!validarCpf(cpf)){
+            System.out.println("O cpf tem que ter 11 dígitos numéricos");
+            return null;
+        }
         int agencia = gerarAgencia();
         Conta conta = new Conta(nome, cpf, agencia);
         contas.add(conta);
         System.out.println("Conta cadastrada com sucesso! Número: " + agencia);
         return conta;
     }
-    public Conta buscarPorAgencia(int agencia) {
+
+    public static boolean validarCpf(String cpf){
+        cpf = cpf.replaceAll("[^0-9]", "");
+        return cpf.matches("\\d{11}");
+    }
+
+    public Conta buscarPorNumero(int agencia) {
         for (Conta c : contas) {
             if (c.getAgencia() == agencia) {
                 return c;
@@ -27,12 +37,15 @@ public class Banco {
         }
         return null;
     }
+
     private int gerarAgencia() {
-        Random random = new Random();
-        int numero;
-        do {
-            numero = random.nextInt(9000) + 1000;
-        } while (buscarPorAgencia(numero) != null);
-        return numero;
-    }
+    Random random = new Random();
+    int number;
+    do
+    {
+         number = random.nextInt(9000) + 1000;
+    } while(buscarPorNumero(number) != null);
+    return number;
+
+}
 }
--- src/main/java/com/Ryan/banco_digital/model/BancoDigitalApplication.java
+++ src/main/java/com/Ryan/banco_digital/model/BancoDigitalApplication.java
@@ -1,13 +1,68 @@
 package com.Ryan.banco_digital.model;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BancoDigitalApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BancoDigitalApplication.class, args);
+		Banco banco = new Banco();
+		Scanner scanner = new Scanner(System.in);
+		int choice;
+
+		System.out.println("Bem vindo ao Banco Digital em JAVA");
+		System.out.println("-----------------------------------");
+		System.out.println("Por favor digite seu nome");
+		String nome = scanner.nextLine();
+		System.out.println("Por favor digite seu cpf");
+		String cpf = scanner.nextLine();
+
+		Conta contaLogada = banco.cadastrarConta(nome,cpf);
+
+		do {
+			System.out.println("Escolha uma opção");
+			System.out.println("---------------------------------");
+			System.out.println("1.Mostrar saldo");
+			System.out.println("2.Depósitar");
+			System.out.println("3.Sacar");
+			System.out.println("4.Transferir para outra conta");
+			System.out.println("5.Sair");
+			System.out.println("---------------------------------");
+
+			choice = scanner.nextInt();
+
+			switch (choice) {
+				case 1:
+					System.out.println("O saldo da conta é: R$ " + contaLogada.getSaldo());
+					break;
+				case 2:
+					System.out.println("Valor do depósito: R$ ");
+					double valorDep = scanner.nextDouble();
+					contaLogada.depositar(valorDep);
+					break;
+				case 3:
+					System.out.println("Qual será o valor do saque: R$ ");
+					double valorSaq = scanner.nextDouble();
+					contaLogada.sacar(valorSaq);
+					break;
+				case 4:
+					System.out.println("Digite o numero da agencia da conta destino: ");
+					int contaDestino = scanner.nextInt();
+					Conta destino = banco.buscarPorNumero(contaDestino);
+					if (destino != null) {
+						System.out.println("Digite valor da transferência: R$ ");
+						double valorTraf = scanner.nextDouble();
+						contaLogada.transferir(destino, valorTraf);
+					} else {
+						System.out.println("Conta não encontrada");
+					}
+
+			}
+		}while (choice != 5);
+		System.out.println("Tenha um bom dia!");
+
+		scanner.close();
 	}
 
 }
